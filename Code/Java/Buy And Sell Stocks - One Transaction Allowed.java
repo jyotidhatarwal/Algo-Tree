030685fdc11fd19249c462252f64bc36fd5faaed
Jyoti Dhatarwal <65608438+jyotidhatarwal@users.noreply.github.com>
2021-03-31 22:07:00 +0530

    Update Buy And Sell Stocks - One Transaction Allowed.java

--- Code/Java/Buy And Sell Stocks - One Transaction Allowed.java
+++ Code/Java/Buy And Sell Stocks - One Transaction Allowed.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         // write your code here
-        Scanner scn = new Scanner(System.in);
+        Scanner scn = new Scanner(System.in);   // Taking inputs
         int n = scn.nextInt();
         int[] prices = new int[n];
         for(int i=0;i<n;i++){
@@ -15,15 +15,15 @@ public class Main {
         
     }
     public static void Stock(int n,int[] prices){
-        int leastSoFar = Integer.MAX_VALUE;
-        int overallProfit=0;
-        int profitIfSoldToday =0;
+        int leastSoFar = Integer.MAX_VALUE; // Maintaing a least i.e stock when price is least
+        int overallProfit=0;        // overall Profit
+        int profitIfSoldToday =0;   // Amount of profit if stock is sold today
         
         for(int i=0;i<prices.length;i++){
         if(prices[i] < leastSoFar){
             leastSoFar = prices[i];
         }
-        profitIfSoldToday = prices[i] - leastSoFar;
+        profitIfSoldToday = prices[i] - leastSoFar; // profit if sold today will be equal to price of stock on today minus least price till now when share is bought
         if(profitIfSoldToday > overallProfit){
             overallProfit = profitIfSoldToday;
         }
